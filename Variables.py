21a130cd12c1c6495e411a5c0d5bcc798e8ef6e8
Hope <hopegiometti@gmail.com>
2020-07-22 11:57:13 -0400

    variables

---
+++ Variables.py
@@ -0,0 +1,21 @@
+#no need to put a let,const,etc. before a variable name
+
+age = 20
+print(age)
+
+sentence = "my name is Hope"
+print(sentence)
+
+#multiple assignment
+sarah,bob,mike = 16,21,17
+print(mike)
+
+fred = george = 25
+print(fred)
+
+#challenge
+#one line, two varaibles
+# --> age, string
+name,age = "Hope", 23 
+print(age)
+
