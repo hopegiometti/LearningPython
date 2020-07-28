56423190a7fc96aea873ac2d822a916be542989a
Hope <hopegiometti@gmail.com>
2020-07-28 11:13:09 -0400

    conditionals

---
+++ conditionals.py
@@ -0,0 +1,31 @@
+if (5 > 3) :
+    print("hello")
+
+if (3 < 2) :
+    print("true")
+else:
+    print("not true")
+
+#if and else statement have to be on the same line
+#functions have to be one tab (four spaces) to the right
+
+#relational operators
+#<, >, <=, >=
+# --> single equal is for assignment
+# == is conditional
+# != --> not equal to
+
+
+#elif 
+age = 16
+if (age < 13):
+    print("you are young")
+elif (age >= 13 and age < 18):
+    print("you are a teen")
+else:
+    print ("you are an adult")
+
+#or vs and
+#makes sure both conditions are true
+#or just checks to see if one is true
+
