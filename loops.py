d292f1eaad0ee540881aaa9346f1777e69114258
Hope <hopegiometti@gmail.com>
2020-07-28 11:29:05 -0400

    while loops

--- loops.py
+++ loops.py
@@ -1,5 +1,7 @@
-list1 = ["apples", "bananas", "cherries"]
-tup1 = (10, 11, 7)
+#for loops
+
+# list1 = ["apples", "bananas", "cherries"]
+# tup1 = (10, 11, 7)
 
 # for item in list1:
 #     print(item)
@@ -22,6 +24,44 @@ tup1 = (10, 11, 7)
 #     print(i)
 
 #nested for loops
-for i in range(0,5):
-    for j in range(0,3):
-        print(i*j)
+# for i in range(0,5):
+#     for j in range(0,3):
+#         print(i*j)
+
+#while loops
+
+c = 0
+# while c < 5:
+#     print(c)
+#     c = c + 1
+
+#control statements
+#break
+#terminates the loop
+# while c < 5:
+#     print(c)
+#     if c == 3:
+#         break
+#     c = c + 1
+
+#continue
+#use when you don't want the rest of code in your while loop to run
+#but you want it to continue iterating
+# while c < 5:
+#     c = c + 1
+#     if c == 3:
+#         continue
+#     print(c)
+
+#pass
+#filler null statement
+#if you dont know what you need to write
+#but you know code needs to be there
+#use pass
+while c < 5:
+    c = c + 1
+    if c == 3:
+        pass
+    print(c)
+
+
