7e66fdfd57cb081d36fc35b9910567aef1f8e546
Hope <hopegiometti@gmail.com>
2020-07-28 11:21:08 -0400

    for loops

---
+++ loops.py
@@ -0,0 +1,27 @@
+list1 = ["apples", "bananas", "cherries"]
+tup1 = (10, 11, 7)
+
+# for item in list1:
+#     print(item)
+
+# for item in tup1:
+#     print(item)
+
+#range functions
+#will print 1-9
+#not inclusive
+# for i in range(0,10):
+#     print(i)
+
+#will only print even numbers
+# for i in range(0,11,2):
+#     print(i)
+
+#will print every 5
+# for i in range(0,51,5):
+#     print(i)
+
+#nested for loops
+for i in range(0,5):
+    for j in range(0,3):
+        print(i*j)
