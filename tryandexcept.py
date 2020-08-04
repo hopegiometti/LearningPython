5abafb24403061913c89f68d2b56e07a605d0d7f
Hope <hopegiometti@gmail.com>
2020-08-04 10:51:43 -0400

    try and except

---
+++ tryandexcept.py
@@ -0,0 +1,17 @@
+#used when you're not sure if your code will work
+#like calling from an api
+#if code in try part doesn't work
+#your except case will run
+
+try: 
+    if name > 3: 
+      print("hello")
+except:
+    print("your code is wrong. try again") 
+
+"""
+multiple line
+comment
+woohoo
+
+"""
