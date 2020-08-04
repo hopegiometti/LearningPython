f9dfd99f78bfeff5dad55d3a5ba88590fa0548ac
Hope <hopegiometti@gmail.com>
2020-08-04 11:05:26 -0400

    built in funcs

--- functions.py
+++ functions.py
@@ -23,3 +23,48 @@ def ReturnAdd(num1, num2):
 sum = ReturnAdd(12, 34)
 
 print(sum)
+
+#in-built functions
+
+#abs
+#call it on any positive or negative number
+#will get the absolute value of that number
+abs(34)
+
+#bool
+#bool of zero is false
+#bool of any other number is true
+#bool of none is false
+bool(0)
+
+#dir
+#gives you a list of anything you can do with a specific data type
+#will show you built-in functions of that data type
+dir("hello")
+
+#help
+#takes in a function name
+#will show you what that function does
+ex = "hello"
+help(ex.upper)
+help(ex.splitlines)
+
+#eval
+#run python code in a string format
+ex2 = 'print("hi")'
+eval(ex2)
+
+#exec
+#similar to eval
+#for more complicated code
+exec(ex2)
+
+#converting data types
+a = 1
+str(a)
+#"a"
+float(a)
+#1.0
+int(a)
+#1
+
