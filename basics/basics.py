6a0e05f7544e2295943fe66ea3fb1606682d14d5
Hope <hopegiometti@gmail.com>
2020-08-26 12:54:14 -0600

    list comp

--- basics/basics.py
+++ basics/basics.py
@@ -2,3 +2,13 @@ import datetime
 
 print("The date and time is", datetime.datetime.now())
 # print(datetime.datetime.now())
+
+#list comprehension
+temps = [221, 234, 340, 230]
+
+fixed_temps = []
+
+for temp in temps:
+    fixed_temps.append(temp / 10)
+
+print(fixed_temps)
