6c5661a48e582401f9acc1158aa18b4b6d9a4df9
Hope <hopegiometti@gmail.com>
2020-09-01 11:36:22 -0600

    more funcs

--- basics/basics.py
+++ basics/basics.py
@@ -22,6 +22,29 @@ print("The date and time is", datetime.datetime.now())
 # print(new_temps)
 
 #need to change order for if/else!
-temps = [221, 234, 340, -9999, 230]
-new_temps = [temp / 10 if temp!= -9999 else 0 for temp in temps ]
-print(new_temps)
+# temps = [221, 234, 340, -9999, 230]
+# new_temps = [temp / 10 if temp!= -9999 else 0 for temp in temps ]
+# print(new_temps)
+
+#functions w/ multiple args
+def area(a, b):
+    return a * b
+
+#non keyword args
+#position matters
+print(area(4, 5))
+
+#keyword args
+#position doesn't matter!
+print(area(a=4, b=5))
+
+#funs w/ arbitrary number of non-keyword arg
+#can be anything after *, but practice is args
+def mean(*args):
+    return sum(args) / len(args)
+
+#with keyword argss
+def mean(**kwargs):
+    return kwargs
+
+print(mean(a=2, b=7, c=5))
