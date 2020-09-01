ff78f546b693219462be06fb157bea60f8498baa
Hope <hopegiometti@gmail.com>
2020-09-01 11:06:35 -0600

    list comp with condit

--- basics/basics.py
+++ basics/basics.py
@@ -4,7 +4,7 @@ print("The date and time is", datetime.datetime.now())
 # print(datetime.datetime.now())
 
 #list comprehension
-temps = [221, 234, 340, 230]
+# temps = [221, 234, 340, 230]
 
 # fixed_temps = []
 
@@ -13,5 +13,9 @@ temps = [221, 234, 340, 230]
 
 # print(fixed_temps)
 
-new_temps = [temp / 10 for temp in temps]
+# new_temps = [temp / 10 for temp in temps]
+# print(new_temps)
+
+temps = [221, 234, 340, -9999, 230]
+new_temps = [temp / 10 for temp in temps if temp!= -9999]
 print(new_temps)
