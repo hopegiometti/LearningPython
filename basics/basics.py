25990000cb538c2f17ab1fdaa8fb4f74fe82490e
Hope <hopegiometti@gmail.com>
2020-09-01 11:02:49 -0600

    list comprehension

--- basics/basics.py
+++ basics/basics.py
@@ -6,9 +6,12 @@ print("The date and time is", datetime.datetime.now())
 #list comprehension
 temps = [221, 234, 340, 230]
 
-fixed_temps = []
+# fixed_temps = []
 
-for temp in temps:
-    fixed_temps.append(temp / 10)
+# for temp in temps:
+#     fixed_temps.append(temp / 10)
 
-print(fixed_temps)
+# print(fixed_temps)
+
+new_temps = [temp / 10 for temp in temps]
+print(new_temps)
